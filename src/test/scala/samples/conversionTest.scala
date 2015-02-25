fb3f9539b126c83869485c8f67534f2e789fd95d
Herrantyr <herrantyrgaming@gmail.com>
2015-02-25 17:22:17 +0100

    Added conversion tests

--- src/test/scala/samples/conversionTest.scala
+++ src/test/scala/samples/conversionTest.scala
@@ -2,11 +2,309 @@ package samples
 
 import org.junit._
 import Assert._
+import stapl.core._
+import stapl.templates.general.GeneralTemplates
+import Thesis.Thesisbuild.TreeConverter
+import stapl.core.pdp.PDP
+import stapl.core.pdp.AttributeFinder
+import org.joda.time.LocalDateTime
+
+object ExamplePolicy extends BasicPolicy with GeneralTemplates{
+  
+  environment.currentDateTime = SimpleAttribute(DateTime)
+  resource.type_ = SimpleAttribute(String)
+  resource.owner_withdrawn_consents = ListAttribute(String)
+  resource.operator_triggered_emergency = SimpleAttribute(Bool)
+  resource.indicates_emergency = SimpleAttribute(Bool)
+  resource.owner_id = SimpleAttribute("owner:id", String)
+  resource.owner_responsible_physicians = ListAttribute("owner:responsible_physicians", String)
+  resource.owner_discharged = SimpleAttribute("owner:discharged", Bool)
+  resource.owner_discharged_dateTime = SimpleAttribute("owner:discharged_dateTime", DateTime)
+  resource.patient_status = SimpleAttribute(String)
+  resource.created = SimpleAttribute(DateTime)
+  subject.roles = ListAttribute(String)
+  subject.triggered_breaking_glass = SimpleAttribute(Bool)
+  subject.department = SimpleAttribute(String)
+  subject.current_patient_in_consultation = SimpleAttribute(String)
+  subject.treated_in_last_six_months = ListAttribute(String)
+  subject.primary_patients = ListAttribute(String)
+  subject.is_head_physician = SimpleAttribute(Bool)
+  subject.treated = ListAttribute(String)
+  subject.treated_by_team = ListAttribute(String)
+  subject.admitted_patients_in_care_unit = ListAttribute(String)
+  subject.shift_start = SimpleAttribute(DateTime)
+  subject.shift_stop = SimpleAttribute(DateTime)
+  subject.location = SimpleAttribute(String)
+  subject.admitted_patients_in_nurse_unit = ListAttribute(String)
+  subject.allowed_to_access_pms = SimpleAttribute(Bool)
+  subject.responsible_patients = ListAttribute(String)
+    
+    val testPO = OnlyPermitIff("policySet:10")(AlwaysTrue)(
+            (resource.owner_id in subject.admitted_patients_in_nurse_unit) 
+            	& (resource.owner_id in subject.responsible_patients)
+        )
+        
+    val testDO = Policy("policyset:11") := when ("nurse" in subject.roles) apply DenyOverrides to (      
+	      // A patient can only access the PMS if (still) allowed by the hospital (e.g., has 
+    	  // subscribed to the PMS, but is not paying any more).
+	      stapl.core.Rule("policy:23") := deny iff !subject.allowed_to_access_pms,
+	      
+	      // A patient can only view his own status.
+	      stapl.core.Rule("policy:16") := deny iff !(subject.location === "hospital"),
+	      
+	      stapl.core.Rule("policy:25") := permit
+    )
+  
+  val testFA = Policy("policyset:11") := when ("nurse" in subject.roles) apply FirstApplicable to (      
+	      // A patient can only access the PMS if (still) allowed by the hospital (e.g., has 
+    	  // subscribed to the PMS, but is not paying any more).
+	      stapl.core.Rule("policy:23") := deny iff !subject.allowed_to_access_pms,
+	      
+	      stapl.core.Rule("policy:25") := permit
+    )
+  
+  }
 
 @Test
-class conversionTest {
+class conversionTest{
 
+  import ExamplePolicy._
+  
   @Test
-  def testOK() = assertTrue(true)
+  def testPOtoDO() {
+    val converter = new TreeConverter(testPO, null)
+    var pdp = new PDP(converter.convertCA(testPO, DenyOverrides), new AttributeFinder)
+    var res1 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+        
+    pdp = new PDP(testPO, new AttributeFinder)
+    var res2 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+    assert(res1 == res2)
+    
+  }
+  @Test
+  def testPOtoFA() {
+    val converter = new TreeConverter(testPO, null)
+    var pdp = new PDP(converter.convertCA(testPO, FirstApplicable), new AttributeFinder)
+    var res1 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+        
+    pdp = new PDP(testPO, new AttributeFinder)
+    var res2 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+    assert(res1 == res2)
+    
+  }
+  
+  @Test
+  def testDOtoPO() {
+    val converter = new TreeConverter(testDO, null)
+    var pdp = new PDP(converter.convertCA(converter.reduceLeaves(testDO), PermitOverrides), new AttributeFinder)
+    var res1 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+        
+    pdp = new PDP(testDO, new AttributeFinder)
+    var res2 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+    assert(res1 == res2)
+    
+  }
+  @Test
+  def testDOtoFA() {
+    val converter = new TreeConverter(testDO, null)
+    var pdp = new PDP(converter.convertCA(testDO, FirstApplicable), new AttributeFinder)
+    var res1 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+        
+    pdp = new PDP(testDO, new AttributeFinder)
+    var res2 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+    assert(res1 == res2)
+    
+  }
+  
+  @Test
+  def testFAtoPO() {
+    val converter = new TreeConverter(testFA, null)
+    var pdp = new PDP(converter.convertCA(converter.reduceLeaves(testFA), PermitOverrides), new AttributeFinder)
+    var res1 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+        
+    pdp = new PDP(testFA, new AttributeFinder)
+    var res2 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+    assert(res1 == res2)
+    
+  }
+  @Test
+  def testFAtoDO() {
+    val converter = new TreeConverter(testFA, null)
+    var pdp = new PDP(converter.convertCA(testFA, DenyOverrides), new AttributeFinder)
+    var res1 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+        
+    pdp = new PDP(testFA, new AttributeFinder)
+    var res2 = pdp.evaluate("maarten", "view", "doc123",
+        subject.roles -> List("medical_personnel", "nurse"),
+        subject.triggered_breaking_glass -> false,
+        subject.department -> "elder_care",
+        subject.allowed_to_access_pms -> true,
+        subject.shift_start -> new LocalDateTime(2014, 6, 24, 9, 0, 0),
+        subject.shift_stop -> new LocalDateTime(2014, 6, 24, 17, 0, 0),
+        subject.location -> "hospital",
+        subject.admitted_patients_in_nurse_unit -> List("patientX", "patientY"),
+        subject.responsible_patients -> List("patientX", "patientZ"),
+        resource.owner_id -> "patientX",
+        resource.owner_withdrawn_consents -> List("subject1"),
+        resource.type_ -> "patientstatus",
+        resource.created -> new LocalDateTime(2014, 6, 22, 14, 2, 1), // three days ago
+        environment.currentDateTime -> new LocalDateTime(2014, 6, 24, 14, 2, 1))
+    assert(res1 == res2)
+    
+  }
   
 }
