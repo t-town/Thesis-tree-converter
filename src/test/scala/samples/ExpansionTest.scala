62e7dda6ad1f76a665c610c709673171fb6c1c4e
Herrantyr <herrantyrgaming@gmail.com>
2015-03-10 16:52:52 +0100

    API expansion test

---
+++ src/test/scala/samples/ExpansionTest.scala
@@ -0,0 +1,59 @@
+package samples
+
+import org.junit._
+import Assert._
+
+@Test
+class ExpansionTest {
+
+  @Test
+  def expansionTest() = {
+    
+  }
+  
+  @Test
+  def canBeSplitTest() = {
+    
+  }
+  
+  @Test
+  def splitPolicyTest() = {
+    
+  }
+  
+  @Test
+  def splitRulesTest() = {
+    
+  }
+  
+  @Test
+  def splitTest() = {
+    
+  }
+  
+  @Test
+  def findCommonTest() = {
+    
+  }
+  
+  @Test
+  def findCommonsTest() = {
+    
+  }
+  
+  @Test
+  def removeCommonTest() = {
+    
+  }
+  
+  @Test
+  def splitOrTest() = {
+    
+  }
+  
+  @Test
+  def nbKnownAttributesTest() = {
+    
+  }
+  
+}
